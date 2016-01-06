1626e5ea1a084af0b24e1127e9f2e6bb94b4b020
Sebastiaan Deckers <seb@ninja.sg>
2016-01-06 10:54:57 +0800

    Routes

--- app.js
+++ app.js
@@ -1,28 +1,3 @@
+var routes = require('./routes')
 var Router = require('spa-quick-router')
-
-var router = new Router([
-  {
-    route: /\/about/,
-    title: 'About Us',
-    element: '#about'
-  },
-  {
-    route: /\/shop/,
-    title: 'SG50 Shop',
-    element: '#products'
-  },
-  {
-    route: /\/shop\/(.+)/,
-    title: 'SG50 Product Details',
-    element: '#details'
-  },
-  {
-    route: /\//,
-    title: 'Homepage',
-    element: '#landing'
-  },
-  {
-    route: /^\/.+$/,
-    element: '#error404'
-  }
-])
+var router = new Router(routes)
